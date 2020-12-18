d7e8b1cbf515de8b8df200004f84a297c72ddd49
Chege Bryan <cbbryan8@gmail.com>
2020-12-18 10:20:58 +0300

    Format bottom navigation bar item code

--- lib/ui/views/home.dart
+++ lib/ui/views/home.dart
@@ -15,25 +15,29 @@ class _HomePageState extends State<HomePage> {
         bottomNavigationBar: BottomNavigationBar(
           items: const <BottomNavigationBarItem>[
             BottomNavigationBarItem(
-                icon: Icon(
-                  Icons.home_outlined,
-                ),
-                label: 'Home'),
+              icon: Icon(
+                Icons.home_outlined,
+              ),
+              label: 'Home',
+            ),
             BottomNavigationBarItem(
-                icon: Icon(
-                  Icons.favorite_border_outlined,
-                ),
-                label: 'Favorites'),
+              icon: Icon(
+                Icons.favorite_border_outlined,
+              ),
+              label: 'Favorites',
+            ),
             BottomNavigationBarItem(
-                icon: Icon(
-                  Icons.shopping_cart_outlined,
-                ),
-                label: 'Cart'),
+              icon: Icon(
+                Icons.shopping_cart_outlined,
+              ),
+              label: 'Cart',
+            ),
             BottomNavigationBarItem(
-                icon: Icon(
-                  Icons.person_outline_outlined,
-                ),
-                label: 'Profile'),
+              icon: Icon(
+                Icons.person_outline_outlined,
+              ),
+              label: 'Profile',
+            ),
           ],
           showSelectedLabels: false,
         ) // This trailing comma makes auto-formatting nicer for build methods.
