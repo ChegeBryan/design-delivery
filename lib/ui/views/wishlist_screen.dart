8798e2c4165b37e42b9ced15a41e2b01de5174c6
Chege Bryan <cbbryan8@gmail.com>
2020-12-21 19:37:56 +0300

    Add wishlist product image

--- lib/ui/views/wishlist_screen.dart
+++ lib/ui/views/wishlist_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:transparent_image/transparent_image.dart';
 
 class WishlistScreen extends StatefulWidget {
   @override
@@ -13,8 +14,23 @@ class _WishlistScreenState extends State<WishlistScreen> {
         title: Text('Wishlist Page Screen'),
         backgroundColor: Theme.of(context).scaffoldBackgroundColor,
       ),
-      body: Center(
-        child: Text('Test'),
+      body: ListView.builder(
+        itemBuilder: (BuildContext context, int) => Row(
+          children: [
+            SizedBox(
+              height: 100.0,
+              width: 100.0,
+              child: Card(
+                clipBehavior: Clip.antiAlias,
+                child: FadeInImage.memoryNetwork(
+                  placeholder: kTransparentImage,
+                  image: 'https://picsum.photos/250?image=9',
+                  fit: BoxFit.cover,
+                ),
+              ),
+            )
+          ],
+        ),
       ),
     );
   }
