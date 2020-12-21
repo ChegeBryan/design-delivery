49492cb3a70da6fba695a244c864fec2f61f7f8c
Chege Bryan <cbbryan8@gmail.com>
2020-12-21 10:34:55 +0300

    Add profile test page

---
+++ lib/ui/views/user_profile_screen.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+class UserProfileScreen extends StatefulWidget {
+  @override
+  _UserProfileScreenState createState() => _UserProfileScreenState();
+}
+
+class _UserProfileScreenState extends State<UserProfileScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Profile Page Screen'),
+      ),
+      body: Center(
+        child: Text('Test.'),
+      ),
+    );
+  }
+}
