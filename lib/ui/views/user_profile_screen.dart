1024e892b7dde463ae3f60390852a10cda701c32
Chege Bryan <cbbryan8@gmail.com>
2020-12-21 21:36:56 +0300

    Add manage addresses listtile

--- lib/ui/views/user_profile_screen.dart
+++ lib/ui/views/user_profile_screen.dart
@@ -28,6 +28,15 @@ class _UserProfileScreenState extends State<UserProfileScreen> {
                 Divider(
                   indent: 52.0,
                 ),
+                ListTile(
+                  leading: Icon(Icons.pin_drop),
+                  title: Text('Manage Addresses'),
+                  trailing: Icon(Icons.keyboard_arrow_right),
+                  onTap: () {},
+                ),
+                Divider(
+                  indent: 52.0,
+                ),
                 TextButton(
                   onPressed: () {},
                   child: Text(
