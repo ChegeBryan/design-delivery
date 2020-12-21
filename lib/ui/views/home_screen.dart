38881b7e746527fa5a6560ac1d9d19a28c00276d
Chege Bryan <cbbryan8@gmail.com>
2020-12-21 16:22:45 +0300

    Add catergory section title

--- lib/ui/views/home_screen.dart
+++ lib/ui/views/home_screen.dart
@@ -9,11 +9,35 @@ class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Home Page Screen'),
-      ),
-      body: Center(
-        child: Text('Test'),
+      body: ListView(
+        shrinkWrap: true,
+        children: [
+          Container(
+            padding: const EdgeInsets.symmetric(vertical: 16.0),
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                Padding(
+                  padding: const EdgeInsets.only(left: 16.0, right: 16.0),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Text(
+                        'Categories',
+                        style: TextStyle(
+                          color: Color(0xFF254081),
+                          fontWeight: FontWeight.bold,
+                          fontSize: 18.0,
+                          fontFamily: 'Lato',
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ],
       ),
     );
   }
