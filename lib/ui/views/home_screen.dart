9fab439fbea9dc6c3c0c75834c8ab23e57d7b73a
Chege Bryan <cbbryan8@gmail.com>
2020-12-21 19:06:09 +0300

    Extract categories widget

--- lib/ui/views/home_screen.dart
+++ lib/ui/views/home_screen.dart
@@ -1,6 +1,6 @@
 import 'package:design_delivery/ui/widgets/home_carousel.dart';
+import 'package:design_delivery/ui/widgets/home_categories.dart';
 import 'package:flutter/material.dart';
-import 'package:transparent_image/transparent_image.dart';
 
 class HomeScreen extends StatefulWidget {
   @override
@@ -16,93 +16,7 @@ class _HomeScreenState extends State<HomeScreen> {
         children: [
           HomeCarousel(),
           // category container
-          Container(
-            color: Colors.red,
-            padding: const EdgeInsets.symmetric(vertical: 16.0),
-            child: Column(
-              mainAxisSize: MainAxisSize.min,
-              children: [
-                Padding(
-                  padding: const EdgeInsets.only(left: 16.0, right: 16.0),
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      Text(
-                        'Categories',
-                        style: TextStyle(
-                          color: Color(0xFF254081),
-                          fontWeight: FontWeight.bold,
-                          fontSize: 18.0,
-                          fontFamily: 'Lato',
-                        ),
-                      ),
-                      TextButton(
-                        onPressed: () {},
-                        child: Text('Shop More'),
-                      ),
-                    ],
-                  ),
-                ),
-                Padding(
-                  padding: const EdgeInsets.only(left: 16.0),
-                  child: SizedBox(
-                    height: 150.0,
-                    child: ListView.builder(
-                      shrinkWrap: true,
-                      scrollDirection: Axis.horizontal,
-                      itemBuilder: (BuildContext context, int index) =>
-                          SizedBox(
-                        height: 150,
-                        width: 120,
-                        child: Card(
-                          clipBehavior: Clip.antiAlias,
-                          child: Column(
-                            crossAxisAlignment: CrossAxisAlignment.start,
-                            children: [
-                              AspectRatio(
-                                aspectRatio: 4 / 3,
-                                child: FadeInImage.memoryNetwork(
-                                  placeholder: kTransparentImage,
-                                  image: 'https://picsum.photos/250?image=9',
-                                  fit: BoxFit.cover,
-                                ),
-                              ),
-                              Padding(
-                                padding: const EdgeInsets.fromLTRB(8, 4, 8, 4),
-                                child: Text(
-                                  'Title ddddddddddd',
-                                  style: TextStyle(
-                                    color: Color(0xFF727C8E),
-                                    fontSize: 16.0,
-                                    fontFamily: 'Lato',
-                                  ),
-                                  softWrap: false,
-                                  overflow: TextOverflow.fade,
-                                ),
-                              ),
-                              Padding(
-                                padding: const EdgeInsets.fromLTRB(8, 4, 8, 4),
-                                child: Text(
-                                  'Ksh. 499.00',
-                                  style: TextStyle(
-                                    color: Color(0xFF727C8E),
-                                    fontSize: 16.0,
-                                    fontWeight: FontWeight.bold,
-                                  ),
-                                  softWrap: false,
-                                  overflow: TextOverflow.fade,
-                                ),
-                              ),
-                            ],
-                          ),
-                        ),
-                      ),
-                    ),
-                  ),
-                )
-              ],
-            ),
-          ),
+          HomeCategories(),
           SizedBox(height: 8.0),
           // Stores container
           Container(
---
+++ lib/ui/widgets/home_categories.dart
@@ -0,0 +1,98 @@
+import 'package:flutter/material.dart';
+import 'package:transparent_image/transparent_image.dart';
+
+class HomeCategories extends StatelessWidget {
+  const HomeCategories({
+    Key key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: Colors.red,
+      padding: const EdgeInsets.symmetric(vertical: 16.0),
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          Padding(
+            padding: const EdgeInsets.only(left: 16.0, right: 16.0),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Text(
+                  'Categories',
+                  style: TextStyle(
+                    color: Color(0xFF254081),
+                    fontWeight: FontWeight.bold,
+                    fontSize: 18.0,
+                    fontFamily: 'Lato',
+                  ),
+                ),
+                TextButton(
+                  onPressed: () {},
+                  child: Text('Shop More'),
+                ),
+              ],
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.only(left: 16.0),
+            child: SizedBox(
+              height: 150.0,
+              child: ListView.builder(
+                shrinkWrap: true,
+                scrollDirection: Axis.horizontal,
+                itemBuilder: (BuildContext context, int index) => SizedBox(
+                  height: 150,
+                  width: 120,
+                  child: Card(
+                    clipBehavior: Clip.antiAlias,
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        AspectRatio(
+                          aspectRatio: 4 / 3,
+                          child: FadeInImage.memoryNetwork(
+                            placeholder: kTransparentImage,
+                            image: 'https://picsum.photos/250?image=9',
+                            fit: BoxFit.cover,
+                          ),
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.fromLTRB(8, 4, 8, 4),
+                          child: Text(
+                            'Title ddddddddddd',
+                            style: TextStyle(
+                              color: Color(0xFF727C8E),
+                              fontSize: 16.0,
+                              fontFamily: 'Lato',
+                            ),
+                            softWrap: false,
+                            overflow: TextOverflow.fade,
+                          ),
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.fromLTRB(8, 4, 8, 4),
+                          child: Text(
+                            'Ksh. 499.00',
+                            style: TextStyle(
+                              color: Color(0xFF727C8E),
+                              fontSize: 16.0,
+                              fontWeight: FontWeight.bold,
+                            ),
+                            softWrap: false,
+                            overflow: TextOverflow.fade,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+              ),
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
