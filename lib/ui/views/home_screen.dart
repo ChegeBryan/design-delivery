0cc9e84df5629d7660c97f36bbdd6d57aff1d5dc
Chege Bryan <cbbryan8@gmail.com>
2020-12-22 18:37:03 +0300

    Remove container margins

--- lib/ui/views/home_screen.dart
+++ lib/ui/views/home_screen.dart
@@ -15,7 +15,6 @@ class _HomeScreenState extends State<HomeScreen> {
     return Scaffold(
       appBar: AppBar(
         title: Container(
-          margin: EdgeInsets.symmetric(horizontal: 12),
           padding: EdgeInsets.symmetric(horizontal: 16),
           decoration: BoxDecoration(
             color: Colors.white,
