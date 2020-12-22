d2c8425acc03de1062f6cca7c6c0c501566b3eac
Chege Bryan <cbbryan8@gmail.com>
2020-12-22 13:43:52 +0300

    Add elevation to container

--- lib/ui/views/cart_screen.dart
+++ lib/ui/views/cart_screen.dart
@@ -15,57 +15,60 @@ class _CartScreenState extends State<CartScreen> {
         backgroundColor: Theme.of(context).scaffoldBackgroundColor,
       ),
       body: CartItems(),
-      bottomNavigationBar: Container(
-        height: 80.0,
-        padding: const EdgeInsets.symmetric(
-          horizontal: 16.0,
-          vertical: 8.0,
-        ),
-        child: Row(
-          crossAxisAlignment: CrossAxisAlignment.end,
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          children: [
-            Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              mainAxisAlignment: MainAxisAlignment.end,
-              children: [
-                Text('Total'),
-                Row(
-                  children: [
-                    Text(
-                      'KSh. 563',
-                      style: TextStyle(
-                        fontSize: 20.0,
-                        fontWeight: FontWeight.bold,
-                        color: Color(0xFF25408F),
+      bottomNavigationBar: Material(
+        elevation: 4.0,
+        child: Container(
+          height: 80.0,
+          padding: const EdgeInsets.symmetric(
+            horizontal: 16.0,
+            vertical: 8.0,
+          ),
+          child: Row(
+            crossAxisAlignment: CrossAxisAlignment.end,
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                mainAxisAlignment: MainAxisAlignment.end,
+                children: [
+                  Text('Total'),
+                  Row(
+                    children: [
+                      Text(
+                        'KSh. 563',
+                        style: TextStyle(
+                          fontSize: 20.0,
+                          fontWeight: FontWeight.bold,
+                          color: Color(0xFF25408F),
+                        ),
                       ),
-                    ),
-                    IconButton(
-                      icon: Icon(Icons.keyboard_arrow_up),
-                      onPressed: () {},
-                      splashRadius: 1,
-                    )
-                  ],
+                      IconButton(
+                        icon: Icon(Icons.keyboard_arrow_up),
+                        onPressed: () {},
+                        splashRadius: 1,
+                      )
+                    ],
+                  ),
+                ],
+              ),
+              ElevatedButton(
+                onPressed: () {},
+                child: Text(
+                  'Checkout',
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontWeight: FontWeight.bold,
+                  ),
                 ),
-              ],
-            ),
-            ElevatedButton(
-              onPressed: () {},
-              child: Text(
-                'Checkout',
-                style: TextStyle(
-                  color: Colors.white,
-                  fontWeight: FontWeight.bold,
+                style: ButtonStyle(
+                  shape: MaterialStateProperty.all(StadiumBorder()),
+                  padding: MaterialStateProperty.all(
+                      EdgeInsets.symmetric(horizontal: 32.0, vertical: 16.0)),
+                  minimumSize: MaterialStateProperty.all(Size(48.0, 8.0)),
                 ),
               ),
-              style: ButtonStyle(
-                shape: MaterialStateProperty.all(StadiumBorder()),
-                padding: MaterialStateProperty.all(
-                    EdgeInsets.symmetric(horizontal: 32.0, vertical: 16.0)),
-                minimumSize: MaterialStateProperty.all(Size(48.0, 8.0)),
-              ),
-            ),
-          ],
+            ],
+          ),
         ),
       ),
     );
