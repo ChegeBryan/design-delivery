46c04c37d626dee6cef1b851689213e74b7a4f52
Chege Bryan <cbbryan8@gmail.com>
2020-12-23 14:12:44 +0300

    Generate login form startup code

---
+++ lib/ui/login/views/login_form.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class LoginForm extends StatefulWidget {
+  @override
+  _LoginFormState createState() => _LoginFormState();
+}
+
+class _LoginFormState extends State<LoginForm> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
