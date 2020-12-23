e9bd11074439dbc957dd6c5dcb9e8a105263b0e3
Chege Bryan <cbbryan8@gmail.com>
2020-12-23 18:50:49 +0300

    Replace safaArea with adding appbar

--- lib/ui/auth/views/login_screen.dart
+++ lib/ui/auth/views/login_screen.dart
@@ -11,48 +11,50 @@ class LoginScreen extends StatefulWidget {
 class _LoginScreenState extends State<LoginScreen> {
   @override
   Widget build(BuildContext context) {
-    return SafeArea(
-      child: Scaffold(
-        resizeToAvoidBottomInset: false,
-        body: Container(
-          padding: const EdgeInsets.all(16.0),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-            children: [
-              CoverImage(),
-              LoginForm(),
-              Center(
-                child: Column(
-                  children: [
-                    Text(
-                      'Dont have an account? Swipe right to',
+    return Scaffold(
+      resizeToAvoidBottomInset: false,
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).scaffoldBackgroundColor,
+        elevation: 0.0,
+      ),
+      body: Container(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            CoverImage(),
+            LoginForm(),
+            Center(
+              child: Column(
+                children: [
+                  Text(
+                    'Dont have an account? Swipe right to',
+                    style: TextStyle(
+                      fontSize: 16.0,
+                    ),
+                  ),
+                  TextButton(
+                    onPressed: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(builder: (context) => Register()),
+                      );
+                    },
+                    child: Text(
+                      'Create a new account.',
                       style: TextStyle(
                         fontSize: 16.0,
                       ),
                     ),
-                    TextButton(
-                      onPressed: () {
-                        Navigator.push(
-                          context,
-                          MaterialPageRoute(builder: (context) => Register()),
-                        );
-                      },
-                      child: Text(
-                        'Create a new account.',
-                        style: TextStyle(
-                          fontSize: 16.0,
-                        ),
-                      ),
-                      style: ButtonStyle(
-                        overlayColor: MaterialStateProperty.all<Color>(
-                            Colors.transparent),
-                      ),
-                    )
-                  ],
-                ),
+                    style: ButtonStyle(
+                      overlayColor:
+                          MaterialStateProperty.all<Color>(Colors.transparent),
+                    ),
+                  )
+                ],
               ),
-            ],
-          ),
+            ),
+          ],
         ),
       ),
     );
