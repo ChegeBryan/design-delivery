a2e6b9d6da690422ed0b22100491fbbdfea1865d
Chege Bryan <cbbryan8@gmail.com>
2020-12-23 17:45:35 +0300

    Fix wrong widget wrapping

--- lib/ui/auth/widgets/register_form.dart
+++ lib/ui/auth/widgets/register_form.dart
@@ -12,60 +12,64 @@ class _RegisterFormState extends State<RegisterForm> {
   Widget build(BuildContext context) {
     return Form(
       key: _formKey,
-      child: Card(
-        child: Column(
-          children: [
-            TextFormField(
-              cursorColor: Colors.amber,
-              keyboardType: TextInputType.text,
-              autocorrect: false,
-              decoration: InputDecoration(
-                prefixIcon: Icon(Icons.person_outline),
-                labelText: 'Username',
-                border: InputBorder.none,
-                contentPadding: EdgeInsets.fromLTRB(0, 8, 8, 8),
-              ),
-              textAlignVertical: TextAlignVertical.center,
+      child: Column(
+        children: [
+          Card(
+            child: Column(
+              children: [
+                TextFormField(
+                  cursorColor: Colors.amber,
+                  keyboardType: TextInputType.text,
+                  autocorrect: false,
+                  decoration: InputDecoration(
+                    prefixIcon: Icon(Icons.person_outline),
+                    labelText: 'Username',
+                    border: InputBorder.none,
+                    contentPadding: EdgeInsets.fromLTRB(0, 8, 8, 8),
+                  ),
+                  textAlignVertical: TextAlignVertical.center,
+                ),
+                Divider(),
+                TextFormField(
+                  cursorColor: Colors.amber,
+                  keyboardType: TextInputType.emailAddress,
+                  autocorrect: false,
+                  decoration: InputDecoration(
+                    prefixIcon: Icon(Icons.email_outlined),
+                    labelText: 'Email',
+                    border: InputBorder.none,
+                    contentPadding: EdgeInsets.fromLTRB(0, 8, 8, 8),
+                  ),
+                  textAlignVertical: TextAlignVertical.center,
+                ),
+                Divider(),
+                TextFormField(
+                  cursorColor: Colors.amber,
+                  decoration: InputDecoration(
+                    prefixIcon: Icon(Icons.lock_outlined),
+                    border: InputBorder.none,
+                    labelText: 'Password',
+                    contentPadding: EdgeInsets.fromLTRB(0, 8, 8, 8),
+                  ),
+                  obscureText: true,
+                  textAlignVertical: TextAlignVertical.center,
+                ),
+                Divider(),
+                TextFormField(
+                  cursorColor: Colors.amber,
+                  decoration: InputDecoration(
+                    prefixIcon: Icon(Icons.lock_outlined),
+                    border: InputBorder.none,
+                    labelText: 'Confirm Password',
+                    contentPadding: EdgeInsets.fromLTRB(0, 8, 8, 8),
+                  ),
+                  obscureText: true,
+                  textAlignVertical: TextAlignVertical.center,
+                ),
+              ],
             ),
-            Divider(),
-            TextFormField(
-              cursorColor: Colors.amber,
-              keyboardType: TextInputType.emailAddress,
-              autocorrect: false,
-              decoration: InputDecoration(
-                prefixIcon: Icon(Icons.email_outlined),
-                labelText: 'Email',
-                border: InputBorder.none,
-                contentPadding: EdgeInsets.fromLTRB(0, 8, 8, 8),
-              ),
-              textAlignVertical: TextAlignVertical.center,
-            ),
-            Divider(),
-            TextFormField(
-              cursorColor: Colors.amber,
-              decoration: InputDecoration(
-                prefixIcon: Icon(Icons.lock_outlined),
-                border: InputBorder.none,
-                labelText: 'Password',
-                contentPadding: EdgeInsets.fromLTRB(0, 8, 8, 8),
-              ),
-              obscureText: true,
-              textAlignVertical: TextAlignVertical.center,
-            ),
-            Divider(),
-            TextFormField(
-              cursorColor: Colors.amber,
-              decoration: InputDecoration(
-                prefixIcon: Icon(Icons.lock_outlined),
-                border: InputBorder.none,
-                labelText: 'Confirm Password',
-                contentPadding: EdgeInsets.fromLTRB(0, 8, 8, 8),
-              ),
-              obscureText: true,
-              textAlignVertical: TextAlignVertical.center,
-            ),
-          ],
-        ),
+          ),
+        ],
       ),
     );
   }
