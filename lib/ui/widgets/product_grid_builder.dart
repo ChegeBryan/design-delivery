0b0ed7e88e8b96fd7ecad10013243dbfb9b236c2
Chege Bryan <cbbryan8@gmail.com>
2020-12-24 13:30:48 +0300

    Set grid to 2 items count on crossAxis

--- lib/ui/widgets/product_grid_builder.dart
+++ lib/ui/widgets/product_grid_builder.dart
@@ -9,7 +9,8 @@ class ProductGridBuilder extends StatelessWidget {
   Widget build(BuildContext context) {
     return GridView.builder(
       itemCount: 17, // for test only put product count here
-      gridDelegate: null,
+      gridDelegate:
+          SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 2),
       itemBuilder: null,
     );
   }
