5cb2970d17d1090a5b3d345e398897edb737a070
Chege Bryan <cbbryan8@gmail.com>
2020-12-24 10:02:45 +0300

    Add button buy now button

--- lib/ui/views/product_detail_screen.dart
+++ lib/ui/views/product_detail_screen.dart
@@ -13,6 +13,73 @@ class _ProductDetailScreenState extends State<ProductDetailScreen> {
         title: Text('Details'),
         backgroundColor: Theme.of(context).scaffoldBackgroundColor,
       ),
+      bottomNavigationBar: Material(
+        elevation: 4.0,
+        child: Container(
+          height: 80,
+          padding: const EdgeInsets.symmetric(
+            horizontal: 16.0,
+            vertical: 16.0,
+          ),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            mainAxisAlignment: MainAxisAlignment.end,
+            children: [
+              Row(
+                crossAxisAlignment: CrossAxisAlignment.end,
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    mainAxisAlignment: MainAxisAlignment.end,
+                    children: [
+                      Text('Total'),
+                      Row(
+                        children: [
+                          Text(
+                            'KSh. 563',
+                            style: TextStyle(
+                              fontSize: 20.0,
+                              fontWeight: FontWeight.bold,
+                              color: Color(0xFF25408F),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ],
+                  ),
+                  ElevatedButton(
+                    onPressed: () {},
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceAround,
+                      children: [
+                        Text(
+                          'Buy Now',
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                        Icon(
+                          Icons.arrow_forward,
+                          size: 14,
+                          color: Colors.white,
+                        ),
+                      ],
+                    ),
+                    style: ButtonStyle(
+                      shape: MaterialStateProperty.all(StadiumBorder()),
+                      padding: MaterialStateProperty.all(EdgeInsets.symmetric(
+                          horizontal: 32.0, vertical: 16.0)),
+                      minimumSize: MaterialStateProperty.all(Size(48.0, 8.0)),
+                    ),
+                  ),
+                ],
+              ),
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
