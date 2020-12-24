789e421a1fe67709778710822840519ced4a0a23
Chege Bryan <cbbryan8@gmail.com>
2020-12-24 17:33:17 +0300

    Add pstore profile page items

--- lib/ui/store/views/store_profile.dart
+++ lib/ui/store/views/store_profile.dart
@@ -13,6 +13,83 @@ class _StoreProfileState extends State<StoreProfile> {
         title: Text('Profile'),
         backgroundColor: Theme.of(context).scaffoldBackgroundColor,
       ),
+      body: ListView(
+        children: [
+          Container(
+            padding: const EdgeInsets.all(16.0),
+            child: Column(
+              children: [
+                Container(
+                  alignment: Alignment.center,
+                  padding: const EdgeInsets.all(16.0),
+                  child: Column(
+                    children: [
+                      Text(
+                        'Store Name',
+                        style: TextStyle(
+                          color: Color(0xFF25408F),
+                          fontSize: 24.0,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                      Text('store address'),
+                    ],
+                  ),
+                ),
+                ListTile(
+                  leading: Icon(Icons.list),
+                  title: Text('All Products'),
+                  trailing: Icon(Icons.keyboard_arrow_right),
+                  onTap: () {},
+                ),
+                Divider(
+                  indent: 52.0,
+                ),
+                ListTile(
+                  leading: Icon(Icons.money),
+                  title: Text('Pending Orders'),
+                  trailing: Icon(Icons.keyboard_arrow_right),
+                  onTap: () {},
+                ),
+                Divider(
+                  indent: 52.0,
+                ),
+                ListTile(
+                  leading: Icon(Icons.check_box_outlined),
+                  title: Text('Completed Orders'),
+                  trailing: Icon(Icons.keyboard_arrow_right),
+                  onTap: () {},
+                ),
+                Divider(
+                  indent: 52.0,
+                ),
+                ListTile(
+                  leading: Icon(Icons.logout),
+                  title: Text('Logout'),
+                  trailing: Icon(Icons.keyboard_arrow_right),
+                  onTap: () {},
+                ),
+                Divider(
+                  indent: 52.0,
+                ),
+                Container(
+                  width: MediaQuery.of(context).size.width,
+                  height: 50.0,
+                  child: TextButton(
+                    onPressed: () {},
+                    child: Text(
+                      'Delete Account',
+                      style: TextStyle(
+                        color: Colors.red,
+                      ),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
