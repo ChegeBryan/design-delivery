8cec4c1a233cc415ad581822ea83578099888dfe
Chege Bryan <cbbryan8@gmail.com>
2020-12-24 09:30:27 +0300

    Generate product details screen

---
+++ lib/ui/views/product_detail.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class ProductDetailScreen extends StatefulWidget {
+  @override
+  _ProductDetailScreenState createState() => _ProductDetailScreenState();
+}
+
+class _ProductDetailScreenState extends State<ProductDetailScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
