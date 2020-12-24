0c1e061b01cdf3a29a447b24fb7eb4c9a2af1b7c
Chege Bryan <cbbryan8@gmail.com>
2020-12-24 13:35:57 +0300

    Add product image to product card

--- lib/ui/widgets/product_card.dart
+++ lib/ui/widgets/product_card.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:transparent_image/transparent_image.dart';
 
 class ProductCard extends StatelessWidget {
   const ProductCard({
@@ -7,6 +8,21 @@ class ProductCard extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Card();
+    return Card(
+      clipBehavior: Clip.antiAlias,
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+          AspectRatio(
+            aspectRatio: 18.0 / 11.0,
+            child: FadeInImage.memoryNetwork(
+              placeholder: kTransparentImage,
+              image: 'https://picsum.photos/250?image=9',
+              fit: BoxFit.cover,
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
