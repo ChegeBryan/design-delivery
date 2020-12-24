e788eb9141900992a355c7d60b409766d996077f
Chege Bryan <cbbryan8@gmail.com>
2020-12-24 16:02:52 +0300

    Add store home navigation

---
+++ lib/ui/store/views/app_store.dart
@@ -0,0 +1,84 @@
+import 'package:design_delivery/ui/store/widgets/store_tab_navigator.dart';
+import 'package:flutter/material.dart';
+
+class AppStore extends StatefulWidget {
+  @override
+  _AppStoreState createState() => _AppStoreState();
+}
+
+class _AppStoreState extends State<AppStore> {
+  int _currentTab = 0;
+  String _currentPage = 'products';
+
+  List<String> pageKeys = ['products', 'profile'];
+
+  Map<String, GlobalKey<NavigatorState>> _navigatorKeys = {
+    'products': GlobalKey<NavigatorState>(),
+    'profile': GlobalKey<NavigatorState>(),
+  };
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _currentPage = pageKeys[index];
+      _currentTab = index;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return WillPopScope(
+      onWillPop: () async {
+        final isFirstRouteInCurrentTab =
+            !await _navigatorKeys[_currentPage].currentState.maybePop();
+        if (isFirstRouteInCurrentTab) {
+          // if not on the products page
+          if (_currentPage != 'products') {
+            // switch to products page
+            _onItemTapped(0);
+            // back button handled by app
+            return false;
+          }
+        }
+        // let system handle back button if we're on the first route
+        return isFirstRouteInCurrentTab;
+      },
+      child: Scaffold(
+        body: Stack(children: <Widget>[
+          _buildOffstageNavigator('products'),
+          _buildOffstageNavigator('profile'),
+        ]),
+        bottomNavigationBar: BottomNavigationBar(
+          type: BottomNavigationBarType.fixed,
+          items: const <BottomNavigationBarItem>[
+            BottomNavigationBarItem(
+              icon: Icon(Icons.list),
+              activeIcon: Icon(Icons.list),
+              label: 'Products',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(
+                Icons.person_outline_outlined,
+              ),
+              activeIcon: Icon(Icons.person),
+              label: 'Profile',
+            ),
+          ],
+          showUnselectedLabels: true,
+          currentIndex: _currentTab,
+          onTap: _onItemTapped,
+        ),
+      ),
+    );
+  }
+
+  Widget _buildOffstageNavigator(String tabItem) {
+    return Offstage(
+      offstage: _currentPage != tabItem,
+      child: StoreTabNavigator(
+        // keep each navigator key seprate for each tab
+        navigatorKey: _navigatorKeys[tabItem],
+        tabItem: tabItem,
+      ),
+    );
+  }
+}
