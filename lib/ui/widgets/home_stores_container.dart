e1353b000f2a73661f20a3805a09d7a7246de851
Chege Bryan <cbbryan8@gmail.com>
2020-12-24 13:03:34 +0300

    Wrap store card with tappable widget

--- lib/ui/widgets/home_stores_container.dart
+++ lib/ui/widgets/home_stores_container.dart
@@ -1,3 +1,4 @@
+import 'package:design_delivery/ui/views/products_by_store_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:transparent_image/transparent_image.dart';
 
@@ -45,46 +46,54 @@ class HomeStoresContainer extends StatelessWidget {
                 itemBuilder: (BuildContext context, int index) => SizedBox(
                   height: 150,
                   width: 120,
-                  child: Card(
-                    clipBehavior: Clip.antiAlias,
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        AspectRatio(
-                          aspectRatio: 4 / 3,
-                          child: FadeInImage.memoryNetwork(
-                            placeholder: kTransparentImage,
-                            image: 'https://picsum.photos/250?image=9',
-                            fit: BoxFit.cover,
-                          ),
-                        ),
-                        Padding(
-                          padding: const EdgeInsets.fromLTRB(8, 4, 8, 4),
-                          child: Text(
-                            'Title ddddddddddd',
-                            style: TextStyle(
-                              color: Color(0xFF727C8E),
-                              fontSize: 16.0,
-                              fontFamily: 'Lato',
+                  child: InkWell(
+                    onTap: () => Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder: (context) => ProductsByStoreScreen(),
+                      ),
+                    ),
+                    child: Card(
+                      clipBehavior: Clip.antiAlias,
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          AspectRatio(
+                            aspectRatio: 4 / 3,
+                            child: FadeInImage.memoryNetwork(
+                              placeholder: kTransparentImage,
+                              image: 'https://picsum.photos/250?image=9',
+                              fit: BoxFit.cover,
                             ),
-                            softWrap: false,
-                            overflow: TextOverflow.fade,
                           ),
-                        ),
-                        Padding(
-                          padding: const EdgeInsets.fromLTRB(8, 4, 8, 4),
-                          child: Text(
-                            'Ksh. 499.00',
-                            style: TextStyle(
-                              color: Color(0xFF727C8E),
-                              fontSize: 16.0,
-                              fontWeight: FontWeight.bold,
+                          Padding(
+                            padding: const EdgeInsets.fromLTRB(8, 4, 8, 4),
+                            child: Text(
+                              'Title ddddddddddd',
+                              style: TextStyle(
+                                color: Color(0xFF727C8E),
+                                fontSize: 16.0,
+                                fontFamily: 'Lato',
+                              ),
+                              softWrap: false,
+                              overflow: TextOverflow.fade,
                             ),
-                            softWrap: false,
-                            overflow: TextOverflow.fade,
                           ),
-                        ),
-                      ],
+                          Padding(
+                            padding: const EdgeInsets.fromLTRB(8, 4, 8, 4),
+                            child: Text(
+                              'Ksh. 499.00',
+                              style: TextStyle(
+                                color: Color(0xFF727C8E),
+                                fontSize: 16.0,
+                                fontWeight: FontWeight.bold,
+                              ),
+                              softWrap: false,
+                              overflow: TextOverflow.fade,
+                            ),
+                          ),
+                        ],
+                      ),
                     ),
                   ),
                 ),
