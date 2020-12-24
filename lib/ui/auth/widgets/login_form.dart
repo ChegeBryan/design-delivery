5ff158f7ab8ff65a0dcd01bba8799541a47bc786
Chege Bryan <cbbryan8@gmail.com>
2020-12-24 16:03:16 +0300

    Temporary redirect to store pages

--- lib/ui/auth/widgets/login_form.dart
+++ lib/ui/auth/widgets/login_form.dart
@@ -1,4 +1,5 @@
 import 'package:design_delivery/ui/auth/widgets/submit_auth_form_button.dart';
+import 'package:design_delivery/ui/store/views/app_store.dart';
 import 'package:design_delivery/ui/views/app.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
@@ -53,7 +54,7 @@ class _LoginFormState extends State<LoginForm> {
               Navigator.pushAndRemoveUntil(
                 context,
                 MaterialPageRoute(
-                  builder: (context) => App(),
+                  builder: (context) => AppStore(),
                 ),
                 (Route<dynamic> route) => false,
               );
