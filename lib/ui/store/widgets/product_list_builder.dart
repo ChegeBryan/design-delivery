0d18f74bf295ab5edbb9aaeff06518d67bd5ed14
Chege Bryan <cbbryan8@gmail.com>
2020-12-25 13:18:52 +0300

    Add tap listener to product on list

--- lib/ui/store/widgets/product_list_builder.dart
+++ lib/ui/store/widgets/product_list_builder.dart
@@ -63,85 +63,89 @@ class ProductListBuilder extends StatelessWidget {
         padding: const EdgeInsets.only(left: 16.0, right: 16.0, top: 8.0),
         child: Column(
           children: [
-            Row(
-              children: [
-                SizedBox(
-                  height: 100.0,
-                  width: 100.0,
-                  child: Card(
-                    clipBehavior: Clip.antiAlias,
-                    child: FadeInImage.memoryNetwork(
-                      placeholder: kTransparentImage,
-                      image: 'https://picsum.photos/250?image=9',
-                      fit: BoxFit.cover,
-                    ),
-                  ),
-                ),
-                Expanded(
-                  child: Container(
+            InkWell(
+              onTap: () {},
+              child: Row(
+                children: [
+                  SizedBox(
                     height: 100.0,
-                    padding: const EdgeInsets.only(left: 16.0),
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                      children: [
-                        Row(
-                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                          children: [
-                            Expanded(
-                              child: Text(
-                                'Product title with overflow test from here continue',
-                                style: TextStyle(
-                                  fontSize: 18.0,
-                                  fontWeight: FontWeight.w500,
-                                  color: Color(0xFF515C6F),
-                                ),
-                                maxLines: 2,
-                                softWrap: true,
-                                overflow: TextOverflow.ellipsis,
-                              ),
-                            ),
-                            IconButton(
-                              icon: Icon(
-                                Icons.edit,
-                                color: Theme.of(context).primaryColor,
-                              ),
-                              // toggle wishlist status
-                              onPressed: () => _lauchEditProductDialog(context),
-                              padding: const EdgeInsets.fromLTRB(8, 8, 0, 8),
-                              splashRadius: 1,
-                            ),
-                          ],
-                        ),
-                        Row(
-                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                          children: [
-                            Text(
-                              'Ksh. 399.00',
-                              style: TextStyle(
-                                fontSize: 20.0,
-                                fontWeight: FontWeight.bold,
-                                color: Color(0xFF25408F),
-                              ),
-                            ),
-                            IconButton(
-                              icon: Icon(
-                                Icons.delete,
-                                color: Theme.of(context).primaryColor,
-                              ),
-                              // toggle wishlist status
-                              onPressed: () {
-                                _confirmDeleteDialog(context);
-                              },
-                              splashRadius: 1,
-                            ),
-                          ],
-                        ),
-                      ],
+                    width: 100.0,
+                    child: Card(
+                      clipBehavior: Clip.antiAlias,
+                      child: FadeInImage.memoryNetwork(
+                        placeholder: kTransparentImage,
+                        image: 'https://picsum.photos/250?image=9',
+                        fit: BoxFit.cover,
+                      ),
                     ),
                   ),
-                ),
-              ],
+                  Expanded(
+                    child: Container(
+                      height: 100.0,
+                      padding: const EdgeInsets.only(left: 16.0),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                        children: [
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: [
+                              Expanded(
+                                child: Text(
+                                  'Product title with overflow test from here continue',
+                                  style: TextStyle(
+                                    fontSize: 18.0,
+                                    fontWeight: FontWeight.w500,
+                                    color: Color(0xFF515C6F),
+                                  ),
+                                  maxLines: 2,
+                                  softWrap: true,
+                                  overflow: TextOverflow.ellipsis,
+                                ),
+                              ),
+                              IconButton(
+                                icon: Icon(
+                                  Icons.edit,
+                                  color: Theme.of(context).primaryColor,
+                                ),
+                                // toggle wishlist status
+                                onPressed: () =>
+                                    _lauchEditProductDialog(context),
+                                padding: const EdgeInsets.fromLTRB(8, 8, 0, 8),
+                                splashRadius: 1,
+                              ),
+                            ],
+                          ),
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: [
+                              Text(
+                                'Ksh. 399.00',
+                                style: TextStyle(
+                                  fontSize: 20.0,
+                                  fontWeight: FontWeight.bold,
+                                  color: Color(0xFF25408F),
+                                ),
+                              ),
+                              IconButton(
+                                icon: Icon(
+                                  Icons.delete,
+                                  color: Theme.of(context).primaryColor,
+                                ),
+                                // toggle wishlist status
+                                onPressed: () {
+                                  _confirmDeleteDialog(context);
+                                },
+                                splashRadius: 1,
+                              ),
+                            ],
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                ],
+              ),
             ),
             Padding(
               padding: const EdgeInsets.only(top: 8.0),
