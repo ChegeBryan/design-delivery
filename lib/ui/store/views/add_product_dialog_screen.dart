34153b3a502b37123e141ec0f95f8c9bb0d892a6
Chege Bryan <cbbryan8@gmail.com>
2020-12-25 10:43:23 +0300

    Add dialog screen launch

---
+++ lib/ui/store/views/add_product_dialog_screen.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class AddProductDialogScreen extends StatefulWidget {
+  @override
+  _AddProductDialogScreenState createState() => _AddProductDialogScreenState();
+}
+
+class _AddProductDialogScreenState extends State<AddProductDialogScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Add Product'),
+      ),
+    );
+  }
+}
--- lib/ui/store/views/products_screen.dart
+++ lib/ui/store/views/products_screen.dart
@@ -1,3 +1,4 @@
+import 'package:design_delivery/ui/store/views/add_product_dialog_screen.dart';
 import 'package:design_delivery/ui/store/widgets/product_list_builder.dart';
 import 'package:flutter/material.dart';
 
@@ -7,6 +8,17 @@ class ProductsScreen extends StatefulWidget {
 }
 
 class _ProductsScreenState extends State<ProductsScreen> {
+  // opens product adding screen as fullscreen dialog
+  void _lauchAddProductDialog() {
+    Navigator.push(
+      context,
+      MaterialPageRoute(
+        builder: (BuildContext context) => AddProductDialogScreen(),
+        fullscreenDialog: true,
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -15,7 +27,7 @@ class _ProductsScreenState extends State<ProductsScreen> {
         backgroundColor: Theme.of(context).scaffoldBackgroundColor,
         actionsIconTheme: IconThemeData(color: Color(0xFF25408F)),
         actions: [
-          IconButton(icon: Icon(Icons.add), onPressed: () {}),
+          IconButton(icon: Icon(Icons.add), onPressed: _lauchAddProductDialog)
         ],
       ),
       body: ProductListBuilder(),
