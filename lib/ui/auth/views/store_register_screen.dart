8901cd981b477bb77deec778b71121b02f20fc5d
Chege Bryan <cbbryan8@gmail.com>
2020-12-28 12:35:33 +0300

    Add courier register screen link

--- lib/ui/auth/views/store_register_screen.dart
+++ lib/ui/auth/views/store_register_screen.dart
@@ -1,5 +1,5 @@
 import 'package:design_delivery/ui/auth/widgets/cover_image.dart';
-import 'package:design_delivery/ui/auth/widgets/register_form.dart';
+import 'package:design_delivery/ui/auth/widgets/form_bottom_text.dart';
 import 'package:design_delivery/ui/auth/widgets/register_store_form.dart';
 import 'package:flutter/material.dart';
 
@@ -21,9 +21,15 @@ class _StoreRegisterScreenState extends State<StoreRegisterScreen> {
       body: Container(
         padding: const EdgeInsets.symmetric(horizontal: 16.0),
         child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
           children: [
             CoverImage(),
             RegisterStoreForm(),
+            FormBottomText(
+              message: 'Want to make deliveries? Swipe right to',
+              actionMessage: 'Register as a courier',
+              swipeTo: null,
+            ),
           ],
         ),
       ),
