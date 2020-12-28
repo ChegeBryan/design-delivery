a13b26a62740e4f785a472e92b749194875b7abf
Chege Bryan <cbbryan8@gmail.com>
2020-12-28 11:25:55 +0300

    Add link to navigate to store register screen

--- lib/ui/auth/views/register_screen.dart
+++ lib/ui/auth/views/register_screen.dart
@@ -1,3 +1,4 @@
+import 'package:design_delivery/ui/auth/views/store_register_screen.dart';
 import 'package:design_delivery/ui/auth/widgets/cover_image.dart';
 import 'package:design_delivery/ui/auth/widgets/register_form.dart';
 import 'package:flutter/material.dart';
@@ -20,9 +21,41 @@ class _RegisterScreenState extends State<RegisterScreen> {
       body: Container(
         padding: const EdgeInsets.symmetric(horizontal: 16.0),
         child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
           children: [
             CoverImage(),
             RegisterForm(),
+            Center(
+              child: Column(
+                children: [
+                  Text(
+                    'Are you a vendor? Swipe right to',
+                    style: TextStyle(
+                      fontSize: 16.0,
+                    ),
+                  ),
+                  TextButton(
+                    onPressed: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                            builder: (context) => StoreRegisterScreen()),
+                      );
+                    },
+                    child: Text(
+                      'Create a vendor account.',
+                      style: TextStyle(
+                        fontSize: 16.0,
+                      ),
+                    ),
+                    style: ButtonStyle(
+                      overlayColor:
+                          MaterialStateProperty.all<Color>(Colors.transparent),
+                    ),
+                  )
+                ],
+              ),
+            ),
           ],
         ),
       ),
