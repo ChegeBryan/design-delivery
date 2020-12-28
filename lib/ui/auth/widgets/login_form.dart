4ef4e7c7572e73151c466f8f69587986f9da0eec
Chege Bryan <cbbryan8@gmail.com>
2020-12-28 20:02:37 +0300

    Add courier entry page

--- lib/ui/auth/widgets/login_form.dart
+++ lib/ui/auth/widgets/login_form.dart
@@ -1,5 +1,6 @@
 import 'package:design_delivery/ui/auth/widgets/auth_text_field.dart';
 import 'package:design_delivery/ui/auth/widgets/submit_auth_form_button.dart';
+import 'package:design_delivery/ui/courier/views/app_courier.dart';
 import 'package:design_delivery/ui/store/views/app_store.dart';
 import 'package:design_delivery/ui/views/app.dart';
 import 'package:flutter/material.dart';
@@ -42,7 +43,7 @@ class _LoginFormState extends State<LoginForm> {
               Navigator.pushAndRemoveUntil(
                 context,
                 MaterialPageRoute(
-                  builder: (context) => AppStore(),
+                  builder: (context) => AppCourier(),
                 ),
                 (Route<dynamic> route) => false,
               );
---
+++ lib/ui/courier/views/app_courier.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class AppCourier extends StatefulWidget {
+  @override
+  _AppCourierState createState() => _AppCourierState();
+}
+
+class _AppCourierState extends State<AppCourier> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
