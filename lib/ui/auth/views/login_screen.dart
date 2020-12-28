5a738a6c3246e2eedbd9cf308de95814c8319829
Chege Bryan <cbbryan8@gmail.com>
2020-12-28 13:15:41 +0300

    Wrap login screen body in SafeArea widget

--- lib/ui/auth/views/login_screen.dart
+++ lib/ui/auth/views/login_screen.dart
@@ -13,24 +13,21 @@ class _LoginScreenState extends State<LoginScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      resizeToAvoidBottomInset: false,
-      appBar: AppBar(
-        backgroundColor: Theme.of(context).scaffoldBackgroundColor,
-        elevation: 0.0,
-      ),
-      body: Container(
-        padding: const EdgeInsets.all(16.0),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-          children: [
-            CoverImage(),
-            LoginForm(),
-            FormBottomText(
-              message: "Don't have an account? Swipe right to",
-              actionMessage: 'Create a new account',
-              swipeTo: RegisterScreen(),
-            ),
-          ],
+      body: SafeArea(
+        child: Container(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              CoverImage(),
+              LoginForm(),
+              FormBottomText(
+                message: "Don't have an account? Swipe right to",
+                actionMessage: 'Create a new account',
+                swipeTo: RegisterScreen(),
+              ),
+            ],
+          ),
         ),
       ),
     );
