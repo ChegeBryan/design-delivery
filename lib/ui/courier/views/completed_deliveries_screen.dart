acab32ad4e2cdecc4f2c61c8f6c382a3947e98bc
Chege Bryan <cbbryan8@gmail.com>
2020-12-29 10:58:33 +0300

    Add initial courier screens

---
+++ lib/ui/courier/views/completed_deliveries_screen.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+class CompletedDeliveriesScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
---
+++ lib/ui/courier/views/new_deliveries_screen.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class NewDeliveriesScreen extends StatefulWidget {
+  @override
+  _NewDeliveriesScreenState createState() => _NewDeliveriesScreenState();
+}
+
+class _NewDeliveriesScreenState extends State<NewDeliveriesScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
