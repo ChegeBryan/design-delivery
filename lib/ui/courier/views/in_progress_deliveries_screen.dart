a5c4ea367e3044bc0d03cc970454372240c7c110
Chege Bryan <cbbryan8@gmail.com>
2020-12-29 13:39:18 +0300

    Add deliveries in progress appbar

--- lib/ui/courier/views/in_progress_deliveries_screen.dart
+++ lib/ui/courier/views/in_progress_deliveries_screen.dart
@@ -3,6 +3,11 @@ import 'package:flutter/material.dart';
 class InProgressDeliveriesScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Deliveries In Progress'),
+        backgroundColor: Theme.of(context).scaffoldBackgroundColor,
+      ),
+    );
   }
 }
