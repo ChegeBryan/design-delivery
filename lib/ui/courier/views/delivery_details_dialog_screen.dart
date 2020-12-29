c1bdb353e04a1f9772296699c5c3cce0b20fa5e5
Chege Bryan <cbbryan8@gmail.com>
2020-12-29 13:13:28 +0300

    Add location details

--- lib/ui/courier/views/delivery_details_dialog_screen.dart
+++ lib/ui/courier/views/delivery_details_dialog_screen.dart
@@ -1,3 +1,4 @@
+import 'package:design_delivery/ui/courier/widgets/detail_attribute.dart';
 import 'package:flutter/material.dart';
 
 class DeliveryDetailsScreen extends StatelessWidget {
@@ -7,6 +8,42 @@ class DeliveryDetailsScreen extends StatelessWidget {
       appBar: AppBar(
         title: Text('Delivery Details'),
         backgroundColor: Theme.of(context).scaffoldBackgroundColor,
+      ),
+      body: SingleChildScrollView(
+        child: Container(
+          padding: EdgeInsets.all(16.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              SizedBox(
+                height: 100.0,
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  mainAxisAlignment: MainAxisAlignment.spaceAround,
+                  children: <Widget>[
+                    Text(
+                      'Location',
+                      style: TextStyle(
+                        color: Color(0xFF25408F),
+                        fontSize: 16.0,
+                        fontWeight: FontWeight.w900,
+                      ),
+                    ),
+                    DetailAttribute(
+                      detailFor: 'Vendor',
+                      detailText: 'Address goes here',
+                    ),
+                    DetailAttribute(
+                      detailFor: 'Customer',
+                      detailText: 'Address Goes here',
+                    ),
+                  ],
+                ),
+              ),
+              Divider(),
+            ],
+          ),
+        ),
       ),
     );
   }
