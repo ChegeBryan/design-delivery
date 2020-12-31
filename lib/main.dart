fd2b49602002883d8d0a0d7cf8612d587ed74e30
Chege Bryan <cbbryan8@gmail.com>
2020-12-31 16:37:03 +0300

    Initialize firebase at app entry point

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,14 @@
 import 'package:design_delivery/ui/auth/views/auth_page_views.dart';
 import 'package:flutter/material.dart';
 
+import 'package:firebase_core/firebase_core.dart';
+
 import 'ui/auth/views/login_screen.dart';
 import 'ui/views/app.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(MyApp());
 }
 
