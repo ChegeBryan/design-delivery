937f261c6774ba63b6002ace68ad8c3aeed4bf14
Chege Bryan <cbbryan8@gmail.com>
2021-01-04 12:19:16 +0300

    Wrap login screen in authentication provider

--- lib/ui/auth/views/login_screen.dart
+++ lib/ui/auth/views/login_screen.dart
@@ -1,8 +1,10 @@
+import 'package:design_delivery/services/auth.dart';
 import 'package:design_delivery/ui/auth/views/register_screen.dart';
 import 'package:design_delivery/ui/auth/widgets/cover_image.dart';
 import 'package:design_delivery/ui/auth/widgets/form_bottom_text.dart';
 import 'package:design_delivery/ui/auth/widgets/login_form.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 class LoginScreen extends StatefulWidget {
   @override
@@ -12,23 +14,26 @@ class LoginScreen extends StatefulWidget {
 class _LoginScreenState extends State<LoginScreen> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: SafeArea(
-        child: SingleChildScrollView(
-          child: Container(
-            height: MediaQuery.of(context).size.height,
-            padding: const EdgeInsets.all(16.0),
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              children: [
-                CoverImage(),
-                LoginForm(),
-                FormBottomText(
-                  message: "Don't have an account? Swipe right",
-                  actionMessage: 'Create a new account',
-                  swipeTo: RegisterScreen(),
-                ),
-              ],
+    return ChangeNotifierProvider(
+      create: (_) => Authentication(),
+      child: Scaffold(
+        body: SafeArea(
+          child: SingleChildScrollView(
+            child: Container(
+              height: MediaQuery.of(context).size.height,
+              padding: const EdgeInsets.all(16.0),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  CoverImage(),
+                  LoginForm(),
+                  FormBottomText(
+                    message: "Don't have an account? Swipe right",
+                    actionMessage: 'Create a new account',
+                    swipeTo: RegisterScreen(),
+                  ),
+                ],
+              ),
             ),
           ),
         ),
