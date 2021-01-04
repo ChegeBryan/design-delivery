189626d239abfaa5af7742cdef82907cbd6dd6f0
Chege Bryan <cbbryan8@gmail.com>
2021-01-04 13:23:18 +0300

    Add isEmail validator helper method

---
+++ lib/helpers/email_validator.dart
@@ -0,0 +1,6 @@
+bool isEmail(input) {
+  Pattern pattern =
+      r'^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$';
+  RegExp regex = new RegExp(pattern);
+  return regex.hasMatch(input);
+}
