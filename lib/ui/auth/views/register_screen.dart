e37646d8a2a727a2cdbe10591e44a8798aa910f8
Chege Bryan <cbbryan8@gmail.com>
2021-01-04 12:06:45 +0300

    Wrap register screen with authentication provider

--- lib/ui/auth/views/register_screen.dart
+++ lib/ui/auth/views/register_screen.dart
@@ -1,8 +1,10 @@
+import 'package:design_delivery/services/auth.dart';
 import 'package:design_delivery/ui/auth/views/store_register_screen.dart';
 import 'package:design_delivery/ui/auth/widgets/cover_image.dart';
 import 'package:design_delivery/ui/auth/widgets/form_bottom_text.dart';
 import 'package:design_delivery/ui/auth/widgets/register_form.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 class RegisterScreen extends StatefulWidget {
   @override
@@ -12,23 +14,26 @@ class RegisterScreen extends StatefulWidget {
 class _RegisterScreenState extends State<RegisterScreen> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: SafeArea(
-        child: SingleChildScrollView(
-          child: Container(
-            height: MediaQuery.of(context).size.height,
-            padding: const EdgeInsets.all(16.0),
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              children: [
-                CoverImage(),
-                RegisterForm(),
-                FormBottomText(
-                  message: 'Are you a vendor? Swipe right',
-                  actionMessage: 'Create a vendor account',
-                  swipeTo: StoreRegisterScreen(),
-                ),
-              ],
+    return ChangeNotifierProvider<Authentication>(
+      create: (_) => Authentication(),
+      child: Scaffold(
+        body: SafeArea(
+          child: SingleChildScrollView(
+            child: Container(
+              height: MediaQuery.of(context).size.height,
+              padding: const EdgeInsets.all(16.0),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  CoverImage(),
+                  RegisterForm(),
+                  FormBottomText(
+                    message: 'Are you a vendor? Swipe right',
+                    actionMessage: 'Create a vendor account',
+                    swipeTo: StoreRegisterScreen(),
+                  ),
+                ],
+              ),
             ),
           ),
         ),
