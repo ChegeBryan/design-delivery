8b3839c63cf8ca93909abdc6b544640a4a76771c
Chege Bryan <cbbryan8@gmail.com>
2021-01-04 12:03:54 +0300

    Add firbase user account registration

--- lib/services/auth.dart
+++ lib/services/auth.dart
@@ -2,5 +2,22 @@ import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 
 class Authentication with ChangeNotifier {
+  String uid;
+  String get getUid => uid;
+
   final FirebaseAuth auth = FirebaseAuth.instance;
+
+  Future registerUserAccount(
+    String username,
+    String email,
+    String password,
+  ) async {
+    UserCredential userCredential = await FirebaseAuth.instance
+        .createUserWithEmailAndPassword(email: email, password: password);
+    await userCredential.user.updateProfile(displayName: username);
+    User user = userCredential.user;
+    uid = user.uid;
+
+    notifyListeners();
+  }
 }
