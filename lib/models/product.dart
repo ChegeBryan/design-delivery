149b788192fdb011ad59522951e891c1231ff26f
Chege Bryan <cbbryan8@gmail.com>
2021-01-05 09:03:57 +0300

    Add product model

---
+++ lib/models/product.dart
@@ -0,0 +1,23 @@
+class Product {
+  String productName;
+  double price;
+  String category;
+  String description;
+  String productImg;
+
+  Product(
+    this.productName,
+    this.price,
+    this.category,
+    this.description,
+    this.productImg,
+  );
+
+  Map<String, dynamic> toJson() => {
+        'procuctName': productName,
+        'price': price,
+        'category': category,
+        'description': description,
+        'productImg': productImg,
+      };
+}
