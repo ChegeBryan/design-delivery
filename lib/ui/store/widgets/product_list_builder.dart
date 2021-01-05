ba7c152a63c39f6253ecb9fb3c028c72ee25d93f
Chege Bryan <cbbryan8@gmail.com>
2021-01-05 14:36:10 +0300

    Wrap product listview in a future builder

--- lib/ui/store/widgets/product_list_builder.dart
+++ lib/ui/store/widgets/product_list_builder.dart
@@ -1,6 +1,9 @@
+import 'package:design_delivery/services/auth.dart';
+import 'package:design_delivery/services/product.dart';
 import 'package:design_delivery/ui/store/views/edit_product_dialog_screen.dart';
 import 'package:design_delivery/ui/store/views/product_detail_dialog_screen.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 import 'package:transparent_image/transparent_image.dart';
 
 class ProductListBuilder extends StatelessWidget {
@@ -69,107 +72,122 @@ class ProductListBuilder extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return ListView.builder(
-      itemBuilder: (BuildContext context, int) => Padding(
-        padding: const EdgeInsets.only(left: 16.0, right: 16.0, top: 8.0),
-        child: Column(
-          children: [
-            InkWell(
-              onTap: () {
-                _launchProductDetailDialog(context);
-              },
-              child: Row(
-                children: [
-                  SizedBox(
-                    height: 100.0,
-                    width: 100.0,
-                    child: Card(
-                      clipBehavior: Clip.antiAlias,
-                      child: FadeInImage.memoryNetwork(
-                        placeholder: kTransparentImage,
-                        image: 'https://picsum.photos/250?image=9',
-                        fit: BoxFit.cover,
-                      ),
-                    ),
-                  ),
-                  Expanded(
-                    child: Container(
-                      height: 100.0,
-                      padding: const EdgeInsets.only(left: 16.0),
-                      child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+    return FutureBuilder(
+        future: Provider.of<ManageProducts>(context)
+            .fetchProductsByStore(Provider.of<Authentication>(context).getUid),
+        builder: (BuildContext context, AsyncSnapshot snapshot) {
+          if (snapshot.connectionState == ConnectionState.done) {
+            return ListView.builder(
+              itemBuilder: (BuildContext context, int) => Padding(
+                padding:
+                    const EdgeInsets.only(left: 16.0, right: 16.0, top: 8.0),
+                child: Column(
+                  children: [
+                    InkWell(
+                      onTap: () {
+                        _launchProductDetailDialog(context);
+                      },
+                      child: Row(
                         children: [
-                          Row(
-                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                            children: [
-                              Expanded(
-                                child: Text(
-                                  'Product title with overflow test from here continue',
-                                  style: TextStyle(
-                                    fontSize: 18.0,
-                                    fontWeight: FontWeight.w500,
-                                    color: Color(0xFF515C6F),
-                                  ),
-                                  maxLines: 2,
-                                  softWrap: true,
-                                  overflow: TextOverflow.ellipsis,
-                                ),
+                          SizedBox(
+                            height: 100.0,
+                            width: 100.0,
+                            child: Card(
+                              clipBehavior: Clip.antiAlias,
+                              child: FadeInImage.memoryNetwork(
+                                placeholder: kTransparentImage,
+                                image: 'https://picsum.photos/250?image=9',
+                                fit: BoxFit.cover,
                               ),
-                              IconButton(
-                                icon: Icon(
-                                  Icons.edit,
-                                  color: Theme.of(context).primaryColor,
-                                ),
-                                // toggle wishlist status
-                                onPressed: () =>
-                                    _lauchEditProductDialog(context),
-                                padding: const EdgeInsets.fromLTRB(8, 8, 0, 8),
-                                splashRadius: 1,
-                              ),
-                            ],
+                            ),
                           ),
-                          Row(
-                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                            children: [
-                              Text(
-                                'Ksh. 399.00',
-                                style: TextStyle(
-                                  fontSize: 20.0,
-                                  fontWeight: FontWeight.bold,
-                                  color: Color(0xFF25408F),
-                                ),
+                          Expanded(
+                            child: Container(
+                              height: 100.0,
+                              padding: const EdgeInsets.only(left: 16.0),
+                              child: Column(
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                mainAxisAlignment:
+                                    MainAxisAlignment.spaceEvenly,
+                                children: [
+                                  Row(
+                                    mainAxisAlignment:
+                                        MainAxisAlignment.spaceBetween,
+                                    children: [
+                                      Expanded(
+                                        child: Text(
+                                          'Product title with overflow test from here continue',
+                                          style: TextStyle(
+                                            fontSize: 18.0,
+                                            fontWeight: FontWeight.w500,
+                                            color: Color(0xFF515C6F),
+                                          ),
+                                          maxLines: 2,
+                                          softWrap: true,
+                                          overflow: TextOverflow.ellipsis,
+                                        ),
+                                      ),
+                                      IconButton(
+                                        icon: Icon(
+                                          Icons.edit,
+                                          color: Theme.of(context).primaryColor,
+                                        ),
+                                        // toggle wishlist status
+                                        onPressed: () =>
+                                            _lauchEditProductDialog(context),
+                                        padding: const EdgeInsets.fromLTRB(
+                                            8, 8, 0, 8),
+                                        splashRadius: 1,
+                                      ),
+                                    ],
+                                  ),
+                                  Row(
+                                    mainAxisAlignment:
+                                        MainAxisAlignment.spaceBetween,
+                                    children: [
+                                      Text(
+                                        'Ksh. 399.00',
+                                        style: TextStyle(
+                                          fontSize: 20.0,
+                                          fontWeight: FontWeight.bold,
+                                          color: Color(0xFF25408F),
+                                        ),
+                                      ),
+                                      IconButton(
+                                        icon: Icon(
+                                          Icons.delete,
+                                          color: Theme.of(context).primaryColor,
+                                        ),
+                                        // toggle wishlist status
+                                        onPressed: () {
+                                          _confirmDeleteDialog(context);
+                                        },
+                                        splashRadius: 1,
+                                      ),
+                                    ],
+                                  ),
+                                ],
                               ),
-                              IconButton(
-                                icon: Icon(
-                                  Icons.delete,
-                                  color: Theme.of(context).primaryColor,
-                                ),
-                                // toggle wishlist status
-                                onPressed: () {
-                                  _confirmDeleteDialog(context);
-                                },
-                                splashRadius: 1,
-                              ),
-                            ],
+                            ),
                           ),
                         ],
                       ),
                     ),
-                  ),
-                ],
+                    Padding(
+                      padding: const EdgeInsets.only(top: 8.0),
+                      child: Divider(
+                        indent: 116,
+                        endIndent: 16,
+                      ),
+                    )
+                  ],
+                ),
               ),
-            ),
-            Padding(
-              padding: const EdgeInsets.only(top: 8.0),
-              child: Divider(
-                indent: 116,
-                endIndent: 16,
-              ),
-            )
-          ],
-        ),
-      ),
-    );
+            );
+          }
+          return Center(
+            child: CircularProgressIndicator(),
+          );
+        });
   }
 }
