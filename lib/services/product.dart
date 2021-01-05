0d9a8e0b0d942d9422d4792b28eb96ed865ab37a
Chege Bryan <cbbryan8@gmail.com>
2021-01-05 09:40:16 +0300

    Stroe prodcut info in firestore

---
+++ lib/services/product.dart
@@ -0,0 +1,20 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/material.dart';
+import 'package:design_delivery/models/product.dart';
+
+class ManageProducts extends ChangeNotifier {
+  Product product;
+  CollectionReference products =
+      FirebaseFirestore.instance.collection('products');
+
+  Future addProduct(
+    String productName,
+    double price,
+    String category,
+    String description,
+    String productImg,
+  ) async {
+    await products.doc().set(product.toJson());
+    notifyListeners();
+  }
+}
