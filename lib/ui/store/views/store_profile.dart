5e205b0995224d3f6b09e4043542157fdd08756e
Chege Bryan <cbbryan8@gmail.com>
2021-01-07 09:41:45 +0300

    Wrap store profile with user data future builder

--- lib/ui/store/views/store_profile.dart
+++ lib/ui/store/views/store_profile.dart
@@ -1,4 +1,7 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:design_delivery/services/auth.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 class StoreProfile extends StatefulWidget {
   @override
@@ -8,88 +11,95 @@ class StoreProfile extends StatefulWidget {
 class _StoreProfileState extends State<StoreProfile> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Profile'),
-        backgroundColor: Theme.of(context).scaffoldBackgroundColor,
-      ),
-      body: ListView(
-        children: [
-          Container(
-            padding: const EdgeInsets.all(16.0),
-            child: Column(
-              children: [
-                Container(
-                  alignment: Alignment.center,
-                  padding: const EdgeInsets.all(16.0),
-                  child: Column(
-                    children: [
-                      Text(
-                        'Store Name',
-                        style: TextStyle(
-                          color: Color(0xFF25408F),
-                          fontSize: 24.0,
-                          fontWeight: FontWeight.bold,
-                        ),
-                      ),
-                      Text('store address'),
-                    ],
-                  ),
-                ),
-                ListTile(
-                  leading: Icon(Icons.list),
-                  title: Text('All Products'),
-                  trailing: Icon(Icons.keyboard_arrow_right),
-                  onTap: () {},
-                ),
-                Divider(
-                  indent: 52.0,
-                ),
-                ListTile(
-                  leading: Icon(Icons.money),
-                  title: Text('Pending Orders'),
-                  trailing: Icon(Icons.keyboard_arrow_right),
-                  onTap: () {},
-                ),
-                Divider(
-                  indent: 52.0,
-                ),
-                ListTile(
-                  leading: Icon(Icons.check_box_outlined),
-                  title: Text('Completed Orders'),
-                  trailing: Icon(Icons.keyboard_arrow_right),
-                  onTap: () {},
-                ),
-                Divider(
-                  indent: 52.0,
-                ),
-                ListTile(
-                  leading: Icon(Icons.logout),
-                  title: Text('Logout'),
-                  trailing: Icon(Icons.keyboard_arrow_right),
-                  onTap: () {},
-                ),
-                Divider(
-                  indent: 52.0,
-                ),
-                Container(
-                  width: MediaQuery.of(context).size.width,
-                  height: 50.0,
-                  child: TextButton(
-                    onPressed: () {},
-                    child: Text(
-                      'Delete Account',
-                      style: TextStyle(
-                        color: Colors.red,
+    return FutureBuilder<DocumentSnapshot>(
+      future: Provider.of<Authentication>(context)
+          .fetchStoreData(Provider.of<Authentication>(context).getUid),
+      builder:
+          (BuildContext context, AsyncSnapshot<DocumentSnapshot> snapshot) {
+        return Scaffold(
+          appBar: AppBar(
+            title: Text('Profile'),
+            backgroundColor: Theme.of(context).scaffoldBackgroundColor,
+          ),
+          body: ListView(
+            children: [
+              Container(
+                padding: const EdgeInsets.all(16.0),
+                child: Column(
+                  children: [
+                    Container(
+                      alignment: Alignment.center,
+                      padding: const EdgeInsets.all(16.0),
+                      child: Column(
+                        children: [
+                          Text(
+                            'Store Name',
+                            style: TextStyle(
+                              color: Color(0xFF25408F),
+                              fontSize: 24.0,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                          Text('store address'),
+                        ],
                       ),
                     ),
-                  ),
+                    ListTile(
+                      leading: Icon(Icons.list),
+                      title: Text('All Products'),
+                      trailing: Icon(Icons.keyboard_arrow_right),
+                      onTap: () {},
+                    ),
+                    Divider(
+                      indent: 52.0,
+                    ),
+                    ListTile(
+                      leading: Icon(Icons.money),
+                      title: Text('Pending Orders'),
+                      trailing: Icon(Icons.keyboard_arrow_right),
+                      onTap: () {},
+                    ),
+                    Divider(
+                      indent: 52.0,
+                    ),
+                    ListTile(
+                      leading: Icon(Icons.check_box_outlined),
+                      title: Text('Completed Orders'),
+                      trailing: Icon(Icons.keyboard_arrow_right),
+                      onTap: () {},
+                    ),
+                    Divider(
+                      indent: 52.0,
+                    ),
+                    ListTile(
+                      leading: Icon(Icons.logout),
+                      title: Text('Logout'),
+                      trailing: Icon(Icons.keyboard_arrow_right),
+                      onTap: () {},
+                    ),
+                    Divider(
+                      indent: 52.0,
+                    ),
+                    Container(
+                      width: MediaQuery.of(context).size.width,
+                      height: 50.0,
+                      child: TextButton(
+                        onPressed: () {},
+                        child: Text(
+                          'Delete Account',
+                          style: TextStyle(
+                            color: Colors.red,
+                          ),
+                        ),
+                      ),
+                    ),
+                  ],
                 ),
-              ],
-            ),
+              ),
+            ],
           ),
-        ],
-      ),
+        );
+      },
     );
   }
 }
