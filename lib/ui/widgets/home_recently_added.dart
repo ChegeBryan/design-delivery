d4d09b332c3b0bd03165345452cbed0b59c94df5
Chege Bryan <cbbryan8@gmail.com>
2021-01-13 11:09:08 +0300

    Wrap recently added product list builder with a future

--- lib/ui/widgets/home_recently_added.dart
+++ lib/ui/widgets/home_recently_added.dart
@@ -1,5 +1,7 @@
+import 'package:design_delivery/services/product.dart';
 import 'package:design_delivery/ui/views/product_detail_screen.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 import 'package:transparent_image/transparent_image.dart';
 
 class HomeRecentlyAdded extends StatelessWidget {
@@ -9,69 +11,74 @@ class HomeRecentlyAdded extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.only(left: 16.0),
-      child: SizedBox(
-        height: 150.0,
-        child: ListView.builder(
-          shrinkWrap: true,
-          scrollDirection: Axis.horizontal,
-          itemBuilder: (BuildContext context, int index) => SizedBox(
-            height: 150,
-            width: 120,
-            child: GestureDetector(
-              onTap: () => Navigator.push(
-                context,
-                MaterialPageRoute(
-                  builder: (context) => ProductDetailScreen(),
-                ),
-              ),
-              child: Card(
-                clipBehavior: Clip.antiAlias,
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    AspectRatio(
-                      aspectRatio: 4 / 3,
-                      child: FadeInImage.memoryNetwork(
-                        placeholder: kTransparentImage,
-                        image: 'https://picsum.photos/250?image=9',
-                        fit: BoxFit.cover,
-                      ),
+    return FutureBuilder(
+      future: Provider.of<ManageProducts>(context).fetchRecentlyAddedProducts(),
+      builder: (BuildContext context, AsyncSnapshot snapshot) {
+        return Padding(
+          padding: const EdgeInsets.only(left: 16.0),
+          child: SizedBox(
+            height: 150.0,
+            child: ListView.builder(
+              shrinkWrap: true,
+              scrollDirection: Axis.horizontal,
+              itemBuilder: (BuildContext context, int index) => SizedBox(
+                height: 150,
+                width: 120,
+                child: GestureDetector(
+                  onTap: () => Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) => ProductDetailScreen(),
                     ),
-                    Padding(
-                      padding: const EdgeInsets.fromLTRB(8, 4, 8, 4),
-                      child: Text(
-                        'Title ddddddddddd',
-                        style: TextStyle(
-                          color: Color(0xFF727C8E),
-                          fontSize: 16.0,
-                          fontFamily: 'Lato',
+                  ),
+                  child: Card(
+                    clipBehavior: Clip.antiAlias,
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        AspectRatio(
+                          aspectRatio: 4 / 3,
+                          child: FadeInImage.memoryNetwork(
+                            placeholder: kTransparentImage,
+                            image: 'https://picsum.photos/250?image=9',
+                            fit: BoxFit.cover,
+                          ),
                         ),
-                        softWrap: false,
-                        overflow: TextOverflow.fade,
-                      ),
-                    ),
-                    Padding(
-                      padding: const EdgeInsets.fromLTRB(8, 4, 8, 4),
-                      child: Text(
-                        'Ksh. 499.00',
-                        style: TextStyle(
-                          color: Color(0xFF727C8E),
-                          fontSize: 16.0,
-                          fontWeight: FontWeight.bold,
+                        Padding(
+                          padding: const EdgeInsets.fromLTRB(8, 4, 8, 4),
+                          child: Text(
+                            'Title ddddddddddd',
+                            style: TextStyle(
+                              color: Color(0xFF727C8E),
+                              fontSize: 16.0,
+                              fontFamily: 'Lato',
+                            ),
+                            softWrap: false,
+                            overflow: TextOverflow.fade,
+                          ),
                         ),
-                        softWrap: false,
-                        overflow: TextOverflow.fade,
-                      ),
+                        Padding(
+                          padding: const EdgeInsets.fromLTRB(8, 4, 8, 4),
+                          child: Text(
+                            'Ksh. 499.00',
+                            style: TextStyle(
+                              color: Color(0xFF727C8E),
+                              fontSize: 16.0,
+                              fontWeight: FontWeight.bold,
+                            ),
+                            softWrap: false,
+                            overflow: TextOverflow.fade,
+                          ),
+                        ),
+                      ],
                     ),
-                  ],
+                  ),
                 ),
               ),
             ),
           ),
-        ),
-      ),
+        );
+      },
     );
   }
 }
