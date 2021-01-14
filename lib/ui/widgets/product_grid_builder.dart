179dba9fe8fb1a136454e82fdcae1fdc27f4982f
Chege Bryan <cbbryan8@gmail.com>
2021-01-14 16:19:42 +0300

    Build category product from future provider

--- lib/ui/widgets/product_grid_builder.dart
+++ lib/ui/widgets/product_grid_builder.dart
@@ -1,22 +1,29 @@
+import 'package:design_delivery/services/product.dart';
 import 'package:design_delivery/ui/widgets/product_card.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 class ProductGridBuilder extends StatelessWidget {
-  const ProductGridBuilder({
-    Key key,
-    String category,
-  }) : super(key: key);
+  final String category;
+
+  const ProductGridBuilder({Key key, this.category}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     final orientation = MediaQuery.of(context).orientation;
 
-    return GridView.builder(
-      itemCount: 17, // for test only put product count here
-      gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-          crossAxisCount: (orientation == Orientation.portrait) ? 2 : 3),
-      itemBuilder: (BuildContext context, int index) => ProductCard(),
-      padding: const EdgeInsets.all(16.0),
+    return FutureBuilder(
+      future: Provider.of<ManageProducts>(context)
+          .fetchProductsByCategory(category),
+      builder: (BuildContext context, AsyncSnapshot snapshot) {
+        return GridView.builder(
+          itemCount: snapshot.data.length,
+          gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+              crossAxisCount: (orientation == Orientation.portrait) ? 2 : 3),
+          itemBuilder: (BuildContext context, int index) => ProductCard(),
+          padding: const EdgeInsets.all(16.0),
+        );
+      },
     );
   }
 }
