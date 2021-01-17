ebb12c6fde26ced7c0eeadc7b76607da558c63bb
Chege Bryan <cbbryan8@gmail.com>
2021-01-17 17:26:59 +0300

    Add all products grid

--- lib/ui/widgets/all_products_grid.dart
+++ lib/ui/widgets/all_products_grid.dart
@@ -1,4 +1,5 @@
 import 'package:design_delivery/services/product.dart';
+import 'package:design_delivery/ui/widgets/product_card.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
@@ -10,6 +11,8 @@ class AllProductsGrid extends StatefulWidget {
 class _AllProductsGridState extends State<AllProductsGrid> {
   @override
   Widget build(BuildContext context) {
+    final orientation = MediaQuery.of(context).orientation;
+
     return FutureBuilder(
       future: Provider.of<ManageProducts>(context).fetchProducts(),
       builder: (BuildContext context, AsyncSnapshot snapshot) {
@@ -19,6 +22,19 @@ class _AllProductsGridState extends State<AllProductsGrid> {
               child: Text('No products found.'),
             );
           }
+          return GridView.builder(
+            itemCount: snapshot.data.length,
+            gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+              crossAxisCount: (orientation == Orientation.portrait) ? 2 : 3,
+              crossAxisSpacing: 8.0,
+              mainAxisSpacing: 8.0,
+            ),
+            itemBuilder: (BuildContext context, int index) => ProductCard(
+              product: snapshot.data[index].data(),
+              productId: snapshot.data[index].id,
+            ),
+            padding: const EdgeInsets.all(8.0),
+          );
         }
         return Center(
           child: CircularProgressIndicator(),
