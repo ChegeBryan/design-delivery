a6755d64391ba3faef9816c8621262c621c46648
Chege Bryan <cbbryan8@gmail.com>
2021-01-25 15:03:36 +0300

    Build wishlist widget from a future builder

--- lib/ui/widgets/wishlist_product.dart
+++ lib/ui/widgets/wishlist_product.dart
@@ -1,80 +1,101 @@
+import 'package:design_delivery/services/product.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 import 'package:transparent_image/transparent_image.dart';
 
-class WishlistProduct extends StatelessWidget {
-  const WishlistProduct({
+class WishlistProducts extends StatelessWidget {
+  final wishlistData;
+
+  const WishlistProducts({
     Key key,
+    this.wishlistData,
   }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return ListView.builder(
-      itemBuilder: (BuildContext context, int) => Padding(
-        padding: const EdgeInsets.only(left: 16.0, right: 16.0, top: 8.0),
-        child: Column(
-          children: [
-            Row(
-              children: [
-                SizedBox(
-                  height: 100.0,
-                  width: 100.0,
-                  child: Card(
-                    clipBehavior: Clip.antiAlias,
-                    child: FadeInImage.memoryNetwork(
-                      placeholder: kTransparentImage,
-                      image: 'https://picsum.photos/250?image=9',
-                      fit: BoxFit.cover,
-                    ),
-                  ),
-                ),
-                Expanded(
-                  child: Container(
-                    height: 100.0,
-                    padding: const EdgeInsets.symmetric(horizontal: 16.0),
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                      children: [
-                        Text(
-                          'Product title',
-                          style: TextStyle(
-                            fontSize: 18.0,
-                            fontWeight: FontWeight.w500,
-                            color: Color(0xFF515C6F),
+    return FutureBuilder(
+      future: Provider.of<ManageProducts>(context)
+          .fetchProductsInArray(wishlistData),
+      builder: (BuildContext context, AsyncSnapshot snapshot) {
+        if (snapshot.connectionState == ConnectionState.done) {
+          if (snapshot.data.length == 0) {
+            return Center(
+              child: Text('Product no longer available.'),
+            );
+          }
+          return ListView.builder(
+            itemBuilder: (BuildContext context, int) => Padding(
+              padding: const EdgeInsets.only(left: 16.0, right: 16.0, top: 8.0),
+              child: Column(
+                children: [
+                  Row(
+                    children: [
+                      SizedBox(
+                        height: 100.0,
+                        width: 100.0,
+                        child: Card(
+                          clipBehavior: Clip.antiAlias,
+                          child: FadeInImage.memoryNetwork(
+                            placeholder: kTransparentImage,
+                            image: 'https://picsum.photos/250?image=9',
+                            fit: BoxFit.cover,
                           ),
                         ),
-                        Text(
-                          'Ksh. 399.00',
-                          style: TextStyle(
-                            fontSize: 20.0,
-                            fontWeight: FontWeight.bold,
-                            color: Color(0xFF25408F),
+                      ),
+                      Expanded(
+                        child: Container(
+                          height: 100.0,
+                          padding: const EdgeInsets.symmetric(horizontal: 16.0),
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                            children: [
+                              Text(
+                                'Product title',
+                                style: TextStyle(
+                                  fontSize: 18.0,
+                                  fontWeight: FontWeight.w500,
+                                  color: Color(0xFF515C6F),
+                                ),
+                              ),
+                              Text(
+                                'Ksh. 399.00',
+                                style: TextStyle(
+                                  fontSize: 20.0,
+                                  fontWeight: FontWeight.bold,
+                                  color: Color(0xFF25408F),
+                                ),
+                              ),
+                            ],
                           ),
                         ),
-                      ],
+                      ),
+                      IconButton(
+                        icon: Icon(
+                          Icons.favorite,
+                          color: Theme.of(context).primaryColor,
+                        ),
+                        // toggle wishlist status
+                        onPressed: () {},
+                      ),
+                    ],
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.only(top: 8.0),
+                    child: Divider(
+                      indent: 116,
+                      endIndent: 16,
                     ),
-                  ),
-                ),
-                IconButton(
-                  icon: Icon(
-                    Icons.favorite,
-                    color: Theme.of(context).primaryColor,
-                  ),
-                  // toggle wishlist status
-                  onPressed: () {},
-                ),
-              ],
-            ),
-            Padding(
-              padding: const EdgeInsets.only(top: 8.0),
-              child: Divider(
-                indent: 116,
-                endIndent: 16,
+                  )
+                ],
               ),
-            )
-          ],
-        ),
-      ),
+            ),
+          );
+        }
+        return Center(
+          child: CircularProgressIndicator(),
+        );
+      },
     );
   }
 }
