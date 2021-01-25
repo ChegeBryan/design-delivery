ad35f81274c3d5c0d07b646439e04411b051ac15
Chege Bryan <cbbryan8@gmail.com>
2021-01-25 09:30:13 +0300

    Initialize wishlist screen with wishlist data

--- lib/ui/views/wishlist_screen.dart
+++ lib/ui/views/wishlist_screen.dart
@@ -1,5 +1,8 @@
+import 'package:design_delivery/services/auth.dart';
+import 'package:design_delivery/services/wishlist.dart';
 import 'package:design_delivery/ui/widgets/wishlist_product.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 class WishlistScreen extends StatefulWidget {
   @override
@@ -7,6 +10,18 @@ class WishlistScreen extends StatefulWidget {
 }
 
 class _WishlistScreenState extends State<WishlistScreen> {
+  List<String> tempWishlist;
+
+  @override
+  void initState() {
+    super.initState();
+    WishlistProvider wishlistProvider =
+        Provider.of<WishlistProvider>(context, listen: false);
+    wishlistProvider.getWishListItems(
+        Provider.of<Authentication>(context, listen: false).getUid);
+    tempWishlist = List<String>.from(wishlistProvider.getWishlistProducts);
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
