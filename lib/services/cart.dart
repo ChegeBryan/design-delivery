951e48c3985dc2e3d784f2b45bb9d3b9f420b382
Chege Bryan <cbbryan8@gmail.com>
2021-05-11 10:32:49 +0300

    Add method to get cart prodcuts

--- lib/services/cart.dart
+++ lib/services/cart.dart
@@ -1,8 +1,26 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:design_delivery/services/auth.dart';
 import 'package:flutter/material.dart';
 
 class CartProvider extends ChangeNotifier {
-  Authentication auth;
+  Authentication _auth;
 
-  CartProvider(this.auth);
+  CartProvider(this._auth);
+
+  CollectionReference cart = FirebaseFirestore.instance.collection('cart');
+
+  List<String> _cartProducts;
+
+  List<String> get getCartProducts => _cartProducts;
+
+  getCartItems() async {
+    /**
+     * Get user cart returng list of product ids
+     */
+    String user = _auth.getUid;
+    DocumentSnapshot<Map<String, dynamic>> snapshot =
+        await cart.doc(user).get();
+    _cartProducts = List<String>.from(snapshot.data()['products']);
+    notifyListeners();
+  }
 }
