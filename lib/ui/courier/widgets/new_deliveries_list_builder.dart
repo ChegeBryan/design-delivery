b227877a7f0110db2b0daf5771fe451e789a3e45
Chege Bryan <cbbryan8@gmail.com>
2021-05-15 00:21:05 +0300

    Add new deliveries list from server

--- lib/ui/courier/widgets/new_deliveries_list_builder.dart
+++ lib/ui/courier/widgets/new_deliveries_list_builder.dart
@@ -1,6 +1,8 @@
+import 'package:design_delivery/services/orders.dart';
 import 'package:design_delivery/ui/courier/views/delivery_details_dialog_screen.dart';
 import 'package:design_delivery/ui/courier/widgets/detail_attribute.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 class NewDeliveriesListBuilder extends StatelessWidget {
   const NewDeliveriesListBuilder({
@@ -19,36 +21,48 @@ class NewDeliveriesListBuilder extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return ListView.separated(
-      itemCount: 20,
-      itemBuilder: (BuildContext context, int) => SizedBox(
-        height: 100.0,
-        width: MediaQuery.of(context).size.width,
-        child: InkWell(
-          onTap: () {
-            _launchDeliveryDetailsDialog(context);
-          },
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.spaceAround,
-            children: [
-              DetailAttribute(
-                detailFor: 'Vendor Name',
-                detailText: 'Address goes here',
+    return FutureBuilder(
+      future: Provider.of<OrderProvider>(context).getOrders(),
+      builder: (context, snapshot) {
+        if (snapshot.connectionState == ConnectionState.done) {
+          if (snapshot.data.length == 0) {
+            return Center(
+              child: Text('No New Orders.'),
+            );
+          }
+          return ListView.separated(
+            itemCount: snapshot.data.length,
+            itemBuilder: (BuildContext context, int index) => SizedBox(
+              height: 75.0,
+              width: MediaQuery.of(context).size.width,
+              child: InkWell(
+                onTap: () {
+                  _launchDeliveryDetailsDialog(context);
+                },
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.spaceAround,
+                  children: [
+                    DetailAttribute(
+                      detailFor: 'Customer Name',
+                      detailText: snapshot.data[index].data()['customerName'],
+                    ),
+                    DetailAttribute(
+                      detailFor: 'Deliver to',
+                      detailText:
+                          snapshot.data[index].data()['deliveryAddress'],
+                    ),
+                  ],
+                ),
               ),
-              DetailAttribute(
-                detailFor: 'Vendor Address',
-                detailText: 'Address goes here',
-              ),
-              DetailAttribute(
-                detailFor: 'Deliver to',
-                detailText: 'Address Goes here',
-              ),
-            ],
-          ),
-        ),
-      ),
-      separatorBuilder: (context, index) => Divider(),
-      padding: const EdgeInsets.all(16.0),
+            ),
+            separatorBuilder: (context, index) => Divider(),
+            padding: const EdgeInsets.all(16.0),
+          );
+        }
+        return Center(
+          child: CircularProgressIndicator(),
+        );
+      },
     );
   }
 }
