6c843cd203e8656a44a4f616972d809b2784d2d9
Chege Bryan <cbbryan8@gmail.com>
2021-05-15 03:15:20 +0300

    Show completed deliveries

--- lib/ui/courier/widgets/completed_deliveries_list_builder.dart
+++ lib/ui/courier/widgets/completed_deliveries_list_builder.dart
@@ -1,54 +1,89 @@
+import 'package:design_delivery/services/auth.dart';
+import 'package:design_delivery/services/orders.dart';
 import 'package:design_delivery/ui/courier/widgets/detail_attribute.dart';
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+import 'package:provider/provider.dart';
 
 class CompletedDeliveriesListBuilder extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return ListView.separated(
-      itemCount: 10,
-      itemBuilder: (BuildContext context, int) => SizedBox(
-        height: 200.0,
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.spaceAround,
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Text(
-              'Delivery $int',
-              style: TextStyle(
-                color: Color(0xFF25408F),
-                fontSize: 16.0,
-                fontWeight: FontWeight.w900,
+    return FutureBuilder(
+      future: Provider.of<OrderProvider>(context).getCourierCompletedDeliveries(
+          Provider.of<Authentication>(context).getUid),
+      builder: (context, snapshot) {
+        if (snapshot.connectionState == ConnectionState.done) {
+          if (!snapshot.hasData) {
+            return Center(
+              child: Text('Order no longer available'),
+            );
+          }
+          return ListView.separated(
+            itemCount: snapshot.data.length,
+            itemBuilder: (BuildContext context, int index) => SizedBox(
+              height: 200.0,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Text(
+                        'Order',
+                        style: TextStyle(
+                          color: Color(0xFF25408F),
+                          fontSize: 16.0,
+                          fontWeight: FontWeight.w900,
+                        ),
+                      ),
+                      Text(
+                        snapshot.data[index].id,
+                        style: TextStyle(
+                          color: Color(0xFF25408F),
+                          fontSize: 16.0,
+                          fontWeight: FontWeight.w900,
+                        ),
+                      ),
+                    ],
+                  ),
+                  DetailAttribute(
+                    detailFor: 'Deliver to',
+                    detailText: snapshot.data[index].data()['deliveryAddress'],
+                  ),
+                  DetailAttribute(
+                    detailFor: 'Customer',
+                    detailText: snapshot.data[index].data()['customerName'],
+                  ),
+                  DetailAttribute(
+                    detailFor: 'Pick Time',
+                    detailText: DateFormat.yMMMd()
+                        .add_jm()
+                        .format(snapshot.data[index]
+                            .data()['deliveryStartTime']
+                            .toDate())
+                        .toString(),
+                  ),
+                  DetailAttribute(
+                    detailFor: 'Completed Time',
+                    detailText: DateFormat.yMMMd()
+                        .add_jm()
+                        .format(snapshot.data[index]
+                            .data()['deliveryEndTime']
+                            .toDate())
+                        .toString(),
+                  ),
+                ],
               ),
             ),
-            DetailAttribute(
-              detailFor: 'Vendor Address',
-              detailText: 'Address goes here',
-            ),
-            DetailAttribute(
-              detailFor: 'Deliver to',
-              detailText: 'Address Goes here',
-            ),
-            DetailAttribute(
-              detailFor: 'Product Name',
-              detailText: 'Name Goes here',
-            ),
-            DetailAttribute(
-              detailFor: 'Quantity',
-              detailText: 'Value',
-            ),
-            DetailAttribute(
-              detailFor: 'Pick Time',
-              detailText: 'Value',
-            ),
-            DetailAttribute(
-              detailFor: 'Completed Time',
-              detailText: 'Value',
-            ),
-          ],
-        ),
-      ),
-      separatorBuilder: (context, index) => Divider(),
-      padding: const EdgeInsets.all(16.0),
+            separatorBuilder: (context, index) => Divider(),
+            padding: const EdgeInsets.all(16.0),
+          );
+        }
+        return Center(
+          child: CircularProgressIndicator(),
+        );
+      },
     );
   }
 }
